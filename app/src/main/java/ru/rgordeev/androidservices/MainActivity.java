afd652baea12816d490bfe1b3b72f3869d386d6e
Roman Gordeev <roma.gordeev@gmail.com>
2022-02-10 17:24:50 +0300

    init

---
+++ app/src/main/java/ru/rgordeev/androidservices/MainActivity.java
@@ -0,0 +1,29 @@
+package ru.rgordeev.androidservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startStartedService(View view) {
+
+        Intent intent = new Intent(MainActivity.this, MyStartedService.class);
+        intent.putExtra("sleepTime", 10);
+        startService(intent);
+    }
+
+    public void stopStartedService(View view) {
+
+        Intent intent = new Intent(MainActivity.this, MyStartedService.class);
+        stopService(intent);
+    }
+}
